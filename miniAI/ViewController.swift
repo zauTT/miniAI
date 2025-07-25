ddda112068c94bc7d9c9a42b7f3f884dab36fc43
zauTT <giozau1@gmail.com>
2025-07-25 16:51:16 +0400

    Initial Commit

---
+++ miniAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniAI
+//
+//  Created by Giorgi Zautashvili on 25.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
